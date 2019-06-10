681cc34ba421b7463cdbd6f7a1852b51a5c29675
frank.he <frank.he@logicpd.com>
2019-06-10 00:24:30 -0500

    Type hello world on the keyboard.

--- RLCraftNet/Sandbox/Program.cs
+++ RLCraftNet/Sandbox/Program.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Keyboard;
 
 namespace Sandbox
 {
@@ -10,7 +12,30 @@ namespace Sandbox
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Thread.Sleep(3000);
+
+            Keyboard.Keyboard.SendKeyAsInput(System.Windows.Forms.Keys.H);
+            Thread.Sleep(1000);
+            Keyboard.Keyboard.SendKeyAsInput(System.Windows.Forms.Keys.E);
+            Thread.Sleep(1000);
+            Keyboard.Keyboard.SendKeyAsInput(System.Windows.Forms.Keys.L);
+            Thread.Sleep(1000);
+            Keyboard.Keyboard.SendKeyAsInput(System.Windows.Forms.Keys.L);
+            Thread.Sleep(1000);
+            Keyboard.Keyboard.SendKeyAsInput(System.Windows.Forms.Keys.O);
+            Thread.Sleep(1000);
+            Keyboard.Keyboard.SendKeyAsInput(System.Windows.Forms.Keys.Space);
+            Thread.Sleep(1000);
+            Keyboard.Keyboard.SendKeyAsInput(System.Windows.Forms.Keys.W);
+            Thread.Sleep(1000);
+            Keyboard.Keyboard.SendKeyAsInput(System.Windows.Forms.Keys.O);
+            Thread.Sleep(1000);
+            Keyboard.Keyboard.SendKeyAsInput(System.Windows.Forms.Keys.R);
+            Thread.Sleep(1000);
+            Keyboard.Keyboard.SendKeyAsInput(System.Windows.Forms.Keys.L);
+            Thread.Sleep(1000);
+            Keyboard.Keyboard.SendKeyAsInput(System.Windows.Forms.Keys.D);
+
             Console.ReadKey();
         }
     }
