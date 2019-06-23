fa4410ec56a3297fbf135667df6182b030dfa8d3
frank.he <frank.he@logicpd.com>
2019-06-22 22:45:14 -0500

    Add environment project.

---
+++ RLCraftNet/Environments/Models/WoW.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Environments.Models
+{
+    public abstract class WoW : IEnvironment
+    {
+        public void Observe()
+        {
+
+        }
+
+        public void GetReward()
+        {
+
+        }
+
+        public void Act()
+        {
+
+        }
+    }
+}
